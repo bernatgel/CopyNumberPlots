656399bf310994283328aa7a3214ef7ed4e3dab9
Bernat Gel <bernatgel@gmail.com>
2019-02-12 15:55:44 +0100

    Removed a spurious line left from development

--- R/loadSNPData.DNAcopy.R
+++ R/loadSNPData.DNAcopy.R
@@ -1,5 +1,5 @@
 
-snp.data <- segment.smoothed.CNA.object
+
 loadSNPData.DNAcopy <- function(snp.data) {
   if(methods::is(snp.data, "DNAcopy")) { #The results object from DNAcopy includes the input object. Extract it
     snp.data <- snp.data$data
@@ -7,14 +7,14 @@ loadSNPData.DNAcopy <- function(snp.data) {
   if(methods::is(snp.data, "CNA")) { #If it's the input object from DNAcopy, extract the marker intensity values
     samples <- names(snp.data)
     samples <- samples[!(samples %in% c("chrom", "maploc"))]
-    
+
     sample.data <- setNames(data.frame(snp.data[[samples]], stringsAsFactors = FALSE), samples)
-    
+
     snps <-  toGRanges(cbind(chr=snp.data$chrom, start=snp.data$maploc, end=snp.data$maploc, sample.data, stringsAsFactors=FALSE))
-    
-    
-    
+
+
+
     return(snps)
   }
-    
+
 }
