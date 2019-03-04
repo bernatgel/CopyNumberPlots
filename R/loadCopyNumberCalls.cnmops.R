210a2f45241420f1a76948c40e634b55001a4ab3
Unknown <mirimagalo@gmail.com>
2019-03-04 14:41:44 +0100

    Added panelcn.mops example and option to load the data

--- R/loadCopyNumberCalls.cnmops.R
+++ R/loadCopyNumberCalls.cnmops.R
@@ -1,20 +1,49 @@
 #CN.mops
 #ejemplo:
-# data(cn.mops, package = "cn.mops")
-# resCNMOPSEX <- exomecn.mops(exomeCounts)
-# resCNMOPSEX <- calcIntegerCopyNumbers(resCNMOPSEX)
-# 
-# resCNMOPS <- cn.mops(XRanges)
-# resCNMOPS <- calcIntegerCopyNumbers(resCNMOPS)
-# cn.mops.res <- resCNMOPSEX
-# cn.mops.res <- "cn.mops.segmentation.csv"
-# cnv.data <- "x"
-# # rr <- tryCatch(toGRanges("1:1-10"), error = function(e){return(NULL)})
-# # rr
-# cnv.data <- "cn.mops.segmentation.csv"
-# 
-# 
-# loadCopyNumberCalls.cnmops(cn.mops.res = cn.mops.res)  
+
+data(panelcn.mops, package = "panelcn.mops")
+XandCB <- test
+control
+resultlist
+elementMetadata(XandCB) <- cbind(elementMetadata(XandCB), 
+                                 elementMetadata(control))
+result <- panelcn.mops::panelcn.mops(XandCB)
+panelcn.mops::plotBoxplot(result.count,countWindows = countWindows,sampleName = colnames(result))
+
+result.count <- panelcn.mops::runPanelcnMops(XandCB,countWindows = countWindows)
+class(result)
+panelcn.mops::runPanelcnMops()
+sampleNames <- colnames(elementMetadata(test))
+resulttable <- createResultTable(resultlist = resultlist,
+                                 XandCB = XandCB, 
+                                 countWindows = countWindows, 
+                                 sampleNames = sampleNames)
+df <-cnv.data
+loadCopyNumberCalls(cnv.data = cnv.data)
+resulttable<-resulttable[[1]]
+loadCopyNumberCalls.cnmops(cn.mops.res = cnv.data)  
+
+cnv.data <- resulttable[[1]]
+class(resulttable)
+class(cnv.data)
+toGRanges(cnv.data)
+colnames(x = cnv.data)
+################################
+data(cn.mops, package = "cn.mops")
+resCNMOPSEX <- exomecn.mops(exomeCounts)
+resCNMOPSEX <- calcIntegerCopyNumbers(resCNMOPSEX)
+class(resCNMOPSEX)
+resCNMOPS <- cn.mops(XRanges)
+resCNMOPS <- calcIntegerCopyNumbers(resCNMOPS)
+cn.mops.res <- resCNMOPSEX
+cn.mops.res <- "cn.mops.segmentation.csv"
+cnv.data <- "x"
+# rr <- tryCatch(toGRanges("1:1-10"), error = function(e){return(NULL)})
+# rr
+cnv.data <- "cn.mops.segmentation.csv"
+
+cn.mops.res <- cnv.data
+loadCopyNumberCalls.cnmops(cn.mops.res = cn.mops.res)  
 
 
 
@@ -30,6 +59,16 @@ loadCopyNumberCalls.cnmops<- function(cn.mops.res,
   if(methods::is(cn.mops.res, "CNVDetectionResult")){
     segs <- cn.mops::segmentation(cn.mops.res)
     segs$cn <- as.integer(gsub(segs$CN,pattern = "CN",replacement = ""))
+    segs <- loadCopyNumberCalls(cnv.data = segs,
+                                chr.col=chr.col, 
+                                start.col=start.col, 
+                                end.col=end.col, 
+                                cn.col=cn.col, 
+                                segment.value.col = segment.value.col,
+                                genome = genome,
+                                verbose = verbose)
+  }else{
+    #when is a dataframe coming from panelcn.mops
     segs <- loadCopyNumberCalls(cnv.data = segs,
                                 chr.col=chr.col, 
                                 start.col=start.col, 
