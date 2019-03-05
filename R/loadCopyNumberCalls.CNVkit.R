71f4f24329b26c372538ffe3b384fa740e5eaff8
Unknown <mirimagalo@gmail.com>
2019-03-05 18:01:57 +0100

    This function is now specialized in load cns files that is CNVkit output

--- R/loadCopyNumberCalls.CNVkit.R
+++ R/loadCopyNumberCalls.CNVkit.R
@@ -1,35 +1,36 @@
 # loadCopyNumberCalls.CNVkit example.
-cnvkit.file <-"testFiles/example.seg"
-seg<-loadCopyNumberCalls.CNVkit(cnvkit.file)
+cnvkit.file <-"testFiles/cl_seq.cns"
+loadCopyNumberCalls.CNVkit(cnvkit.file)
 # x <- read.table(file=ex.file,header = F, sep = "\t",quote = "\'")
 # colnames(x) <- c("ID","chrom", "loc.start", "loc.end", "num.mark", "seg.mean")
 
 loadCopyNumberCalls.CNVkit <- function(cnvkit.file, 
-                                       chr.col=NULL, 
-                                       start.col=NULL,
-                                       end.col=NULL, 
-                                       segment.value.col = NULL, # we can only have one value use match.arg()
-                                       genome = NULL, verbose=TRUE){
+                                       chr.col = NULL, 
+                                       start.col = NULL,
+                                       end.col = NULL, 
+                                       segment.value.col = "log2", 
+                                       cn.col = NA,
+                                       genome = NULL,
+                                       verbose = TRUE){
   #Load file.seg. First, probe if file exist
   if(!file.exists(cnvkit.file)){
     stop(paste0(cnvkit.file, " does not exist or you are not in the correct directory."))
-    
-  }else{
-    cnvkit.data <- read.table(file = cnvkit.file, sep="\t", skip = 1, stringsAsFactors = FALSE)
-    colnames(cnvkit.data) <-  c("ID","chrom", "loc.start", "loc.end", "num.mark", "seg.mean")
-    cnvkit.data <- cnvkit.data[,c("chrom", "loc.start", "loc.end", "ID", "num.mark", "seg.mean")]
   }
   
-  segs <- loadCopyNumberCalls(cnv.data = cnvkit.data, 
-                                          chr.col=chr.col, 
-                                          start.col=start.col,
-                                          end.col=end.col, 
-                                          cn.col=cn.col, 
-                                          segment.value.col = segment.value.col, 
-                                          genome = genome,
-                                          verbose = verbose)
-  return(seg)
+  segs <- loadCopyNumberCalls(cnv.data = cnvkit.file, 
+                              chr.col = chr.col, 
+                              start.col = start.col,
+                              end.col = end.col, 
+                              cn.col = cn.col, 
+                              segment.value.col = segment.value.col,
+                              loh.col = NA, 
+                              genome = genome,
+                              verbose = verbose)
+  
+  
+  return(segs)
 }
 
-x<-loadCopyNumberCalls.CNVkit(cnvkit.file)
+# by this function its not posible to recognize hg19 as reference genome
+x<-loadCopyNumberCalls.CNVkit(cnvkit.file, genome = "hg19")
 
